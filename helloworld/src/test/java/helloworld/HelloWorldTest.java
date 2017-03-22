61214fb6f855847ad46e9c2c017281261dc836ef
RavisankarCts <ravilink_2000@yahoo.com>
2017-03-22 16:32:57 +0100

    Added test code

---
+++ helloworld/src/test/java/helloworld/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.omg.PortableInterceptor.SUCCESSFUL;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+
+}
